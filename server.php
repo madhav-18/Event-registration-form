5421026c8934358c6fdbecefe285fb47f8467b94
madhav-18 <85945172+madhav-18@users.noreply.github.com>
2022-04-24 14:12:31 +0530

    Update server.php

--- server.php
+++ server.php
@@ -3,6 +3,7 @@
   // initializing variables
   $username = "";
   $email    = "";
+  $code= array();
   $errors = array(); 
 
   // connect to the database
@@ -45,10 +46,26 @@
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
-
+    $activationcode = rand(99999, 11111);
   	$query = "INSERT INTO register_user (username, email, password) 
   			  VALUES('$Username', '$email', '$password')";
   	mysqli_query($db, $query);
+    if ($result) {
+      $last_id = mysqli_insert_id($db);
+      if($last_id){
+        $code = rand(1,9999);
+        $user_id = "UC_".$code."_".$last_id;
+        $query = "Update register_user SET user_id = '".$user_id."' WHERE id = '".$last_id."'";
+        $res = mysqli_query($db,$query);
+      }
+      $to = $email;
+      $subject = "Email Verification Code";
+      $message = "Your verification code is $code";
+      mail($to, $subject, $message);
+      $_SESSION['email'] = "We've sent a verification code to your email - $email!";
+      header("Location: verification.php");
+  }
+
   	$_SESSION['username'] = $Username;
   	$_SESSION['success'] = "You are now logged in";
   	header('location: login.php');
